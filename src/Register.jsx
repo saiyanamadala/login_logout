a4340254a3448211a0cbc45e2be08c0f292d9ebc
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-06-05 21:18:34 -0500

    Working on registration

---
+++ src/Register.jsx
@@ -0,0 +1,71 @@
+import { Field, Form, Formik } from "formik"
+import { useState } from "react"
+
+export default function Register(){
+    
+    const [id,setId]=useState(0);
+    
+
+    function onSubmit(values){
+        const register={
+            username: values.username,
+            password: values.password,
+            birthdate: values.birthdate,
+            phonenumber: values.phonenumber,
+            gender: values.gender,
+            email: values.email
+        }
+        console.log(register);
+    }
+
+    return(
+        <div className="container">
+        <Formik initialValues={{
+                    username:'', 
+                    password:'', 
+                    birthdate:'', 
+                    id:'', 
+                    phonenumber: '', 
+                    gender: '', 
+                    email:'' 
+                }}
+                onSubmit={onSubmit}
+
+                >
+            <Form>
+                <fieldset className="form-group">
+                    <label>Username</label>
+                    <Field type="text" className="form-control" name="username"></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Password</label>
+                    <Field type="password" className="form-control" name="password"></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>BirthDate</label>
+                    <Field type="date" className="form-control" name="birthdate"></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Phone Number</label>
+                    <Field type="tel" className="form-control" name="phonenumber"></Field>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Gender</label>
+                    <Field as="select" className="form-control" name="gender">
+                            <option value="">Select Gender</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                    </Field>
+                </fieldset>
+                <fieldset className="form-group">
+                    <label>Email</label>
+                    <Field type="email" className="form-control" name="email"></Field>
+                </fieldset>
+                <div>
+                    <button className="btn btn-success m-3" type="submit">Register</button>
+                </div>
+            </Form>
+        </Formik>
+        </div>
+    )
+}
